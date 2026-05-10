f65a525893e1de67aac93139a5057cf151b732b2
bobart717-ctrl <bobart717@gmail.com>
2026-05-10 10:31:06 +0300

    Update Drivers.cs

--- Drivers.cs
+++ Drivers.cs
@@ -2,22 +2,20 @@ using System.Runtime.InteropServices;
 
 namespace PlusOS {
     public static class Drivers {
-        // Импортируем наши функции из Ассемблера
         [DllImport("*")] public static extern void outb(ushort port, byte data);
         [DllImport("*")] public static extern byte inb(ushort port);
 
         public static void CheckKeyboard() {
-            // Проверяем, нажата ли клавиша (статус-порт 0x64)
             if ((inb(0x64) & 0x01) != 0) {
-                byte scanCode = inb(0x60); // Читаем скан-код клавиши
+                byte scanCode = inb(0x60);
                 
-                // Пример: если нажат пробел (скан-код 0x39)
-                if (scanCode == 0x39) {
-                    Notepad.TypeChar(' ');
-                }
-                // Если нажат 'N' (скан-код 0x31) - открываем блокнот
+                // Клавиша 'N' открывает блокнот
                 if (scanCode == 0x31) {
                     Notepad.IsOpen = true;
+                }
+                // Клавиша пробел для теста печати
+                if (scanCode == 0x39) {
+                    Notepad.TypeChar(' ');
                 }
             }
         }
