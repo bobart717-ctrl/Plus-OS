c6701d01e55cb3503155dfc3d8d95612e6ab6c24
bobart717-ctrl <bobart717@gmail.com>
2026-05-10 10:26:29 +0300

    Update LowLevel.asm

--- LowLevel.asm
+++ LowLevel.asm
@@ -1,9 +1,18 @@
 [bits 32]
-global _start
-extern Main
+global outb
+global inb
 
 section .text
-_start:
-    call Main
-    cli
-    hlt
+
+; Записать байт в порт: outb(port, data)
+outb:
+    mov dx, [esp + 4]
+    mov al, [esp + 8]
+    out dx, al
+    ret
+
+; Прочитать байт из порта: inb(port)
+inb:
+    mov dx, [esp + 4]
+    in al, dx
+    ret
