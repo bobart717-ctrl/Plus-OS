2776046814a8e6e2fde86e6560d81e78aa88c4ac
bobart717-ctrl <bobart717@gmail.com>
2026-05-10 10:30:02 +0300

    Update Kernel.cs

--- Kernel.cs
+++ Kernel.cs
@@ -1,20 +1,45 @@
+using System;
+
 namespace PlusOS {
     public class Kernel {
+        private static unsafe byte* VidMem = (byte*)0xB8000;
+
         public static void Main() {
             DrawUI();
             
             while (true) {
-                // Вызываем драйвер в каждом цикле
                 Drivers.CheckKeyboard();
                 
                 if (Notepad.IsOpen) {
                     Notepad.Draw();
                 }
-                
-                // Серый квадрат (курсор)
-                WriteAt("█", 10, 20, 0x08);
+                // Твой курсор - серый квадрат
+                WriteAt("█", 10, 20, 0x08); 
             }
         }
-        // ... (остальные методы WriteAt и DrawUI остаются как были)
+
+        public static unsafe void DrawUI() {
+            // Заливка черным
+            for (int i = 0; i < 80 * 25 * 2; i += 2) {
+                VidMem[i] = 0; VidMem[i+1] = 0x00;
+            }
+            // Фиолетовая панель
+            for (int i = 0; i < 80; i++) {
+                WriteAt(" ", 24, i, 0x55); 
+            }
+            // Иконка блокнота
+            WriteAt("■", 24, 2, 0x5F);
+        }
+
+        public static unsafe void WriteAt(string s, int r, int c, byte col) {
+            int off = (r * 80 + c) * 2;
+            if (off >= 0 && off < 80 * 25 * 2) {
+                foreach (char ch in s) {
+                    VidMem[off] = (byte)ch;
+                    VidMem[off + 1] = col;
+                    off += 2;
+                }
+            }
+        }
     }
 }
